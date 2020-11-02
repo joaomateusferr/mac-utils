da4dced08aedae35d3f3affec4691c2723f5ab1e
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-02 13:45:03 -0300

    update

--- scripts/pkg/get_info_single_app_pkg.sh
+++ scripts/pkg/get_info_single_app_pkg.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 #Developer notes
 #Currently this script only get data from pkgs that contains a single .app file if there is more than one the first in alphabetical order will be taken into account
 
-#!/usr/bin/env bash
 PATH_TO_PKG='/Users/joaoferreira/Downloads/Zoom.pkg'
 FILEDESTDMATION='/private/tmp'
 FOLDER_TO_EXTRACT_TO='/private/tmp/extracted_pkg'
@@ -9,8 +10,6 @@ FOLDER_TO_EXTRACT_TO='/private/tmp/extracted_pkg'
 PATH_TO_PKG='/private/tmp/Zoom.pkg'
 
 #cp "$PATH_TO_PKG" "$FILEDESTDMATION" #need to test it in /tmp
-
-#APP VARIABLES
 
 if [ -e "$FOLDER_TO_EXTRACT_TO" ];then
     rm -rf "$FOLDER_TO_EXTRACT_TO"
--- scripts/pkg/get_pkg_payload_content.sh
+++ scripts/pkg/get_pkg_payload_content.sh
@@ -1,17 +1,23 @@
-#!/usr/bin/env bash
-PATH_TO_PKG='/tmp/Zoom.pkg'
-FOLDER_TO_EXTRACT_TO='/tmp/extracted_pkg'
+PATH_TO_PKG='/Users/joaoferreira/Downloads/Zoom.pkg'
+FILEDESTDMATION='/private/tmp'
+FOLDER_TO_EXTRACT_TO='/private/tmp/extracted_pkg'
 
-#APP VARIABLES
+PATH_TO_PKG='/private/tmp/Zoom.pkg'
 
-APP_NAME='zoom.us.app'
-PKG_NAME='zoomus.pkg'
+#cp "$PATH_TO_PKG" "$FILEDESTDMATION" #need to test it in /tmp
 
 if [ -e "$FOLDER_TO_EXTRACT_TO" ];then
     rm -rf "$FOLDER_TO_EXTRACT_TO"
 fi
 
 pkgutil --expand-full $PATH_TO_PKG $FOLDER_TO_EXTRACT_TO
+
+PKG_NAME=$(ls "$FOLDER_TO_EXTRACT_TO" | grep '\.pkg$' | head -1)
+
+if [ -z "$PKG_NAME" ];then
+    echo 'Unable to find the pkg content'
+    exit
+fi
 
 open $FOLDER_TO_EXTRACT_TO/$PKG_NAME/Payload/$APP_NAME
 
