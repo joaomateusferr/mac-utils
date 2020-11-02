09df118572361a40facfa429f43401c45c70e275
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-02 09:44:12 -0300

    update files

--- scripts/change_dns_internal_interfaces.sh
+++ scripts/change_dns_internal_interfaces.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-DNS="8.8.8.8";
+DNS='8.8.8.8'
 
-ISETHERNETAVALABLE=$(networksetup -listallhardwareports | grep -A2 "Hardware Port.*Ethernet");
-ISWIFIAVALABLE=$(networksetup -listallhardwareports | grep -A2 "Hardware Port.*Wi-Fi");
+ISETHERNETAVALABLE=$(networksetup -listallhardwareports | grep -A2 "Hardware Port.*Ethernet")
+ISWIFIAVALABLE=$(networksetup -listallhardwareports | grep -A2 "Hardware Port.*Wi-Fi")
 
 if [ -z "$ISETHERNETAVALABLE" ];then
-    echo "Ethernet not available!";
+    echo 'Ethernet not available!'
 else
-    networksetup -setdnsservers Ethernet "$DNS";
-    ETHERNETSTATUS=$?;
+    networksetup -setdnsservers Ethernet "$DNS"
+    ETHERNETSTATUS=$?
 
     if [ $ETHERNETSTATUS -eq 0 ];then
-        echo "Ethernet DNS changed";
+        echo 'Ethernet DNS changed'
     else
-        echo "Error, ethernet DNS not changed";
+        echo 'Error, ethernet DNS not changed'
     fi
 
 fi
 
 if [ -z "$ISWIFIAVALABLE" ];then
-    echo "Wi-Fi not available!";
+    echo 'Wi-Fi not available!'
 else
-    networksetup -setdnsservers Wi-Fi "$DNS";
-    WIFISTATUS=$?;
+    networksetup -setdnsservers Wi-Fi "$DNS"
+    WIFISTATUS=$?
 
     if [ $WIFISTATUS -eq 0 ];then
-        echo "Wi-Fi DNS changed";
+        echo 'Wi-Fi DNS changed'
     else
-        echo "Error, Wi-Fi DNS not changed";
+        echo 'Error, Wi-Fi DNS not changed'
     fi
-
 fi
