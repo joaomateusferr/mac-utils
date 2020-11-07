13d84e280a98228533baf13b1dfaaac4382b1c58
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-07 11:46:59 -0300

    update

--- scripts/os_update.sh
+++ scripts/os_update.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+
+ERASE_VOLUME=$1
+
+if [ -z $ERASE_VOLUME ];then
+    echo 'Please, use the scripts argument:'
+    echo 'ERASE_VOLUME (1 or 0)'
+    exit 1
+fi
+
 #https://osxdaily.com/2020/04/13/how-download-full-macos-installer-terminal/
-#sudo /Applications/Install\macOS\ Mojave.app --agreetolicense --eraseinstall --newvolumename "Macintosh HD" --nointeraction
-#sudo /Applications/Install\macOS\ Mojave.app --agreetolicense --nointeraction
+softwareupdate --download --fetch-full-installer --full-installer-version 10.13.6 
+exit
 
 APP_NAME='Install macOS Catalina.app'
 APP_NAME_PATH=${APP_NAME// /\\ }
-COMMAND="/Applications/$APP_NAME_PATH/Contents/Resources/startosinstall -h"
-eval $COMMAND
+COMMAND="/Applications/$APP_NAME_PATH/Contents/Resources/startosinstall"
+
+if [ $ERASE_VOLUME -eq 1 ]; then
+    PARAMETERS='--agreetolicense --eraseinstall --newvolumename "Macintosh HD" --nointeraction'
+else
+    PARAMETERS='--agreetolicense --nointeraction'
+fi
+
+eval $COMMAND $PARAMETERS
