60114cf53ba24040fec1dbd5e4e066aea2c2d04c
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-07 09:00:03 -0300

    update

--- scripts/os_update.sh
+++ scripts/os_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #https://osxdaily.com/2020/04/13/how-download-full-macos-installer-terminal/
 #sudo /Applications/Install\macOS\ Mojave.app --agreetolicense --eraseinstall --newvolumename "Macintosh HD" --nointeraction
-sudo /Applications/Install\macOS\ Mojave.app --agreetolicense --nointeraction
+#sudo /Applications/Install\macOS\ Mojave.app --agreetolicense --nointeraction
 
 APP_NAME='Install macOS Catalina.app'
 APP_NAME_PATH=${APP_NAME// /\\ }
