9f2ce29c5046cc074362090f98205fa135b50200
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-07 15:00:49 -0300

    fix add scripts to pkg

--- scripts/pkg/pkgbuilder/build_pkg.sh
+++ scripts/pkg/pkgbuilder/build_pkg.sh
@@ -9,8 +9,6 @@ if [ -z ${BUNDLEID} ] || [ -z ${VERSION} ] ||  [ -z ${BUNDLEISRELOCATABLE} ];the
     exit
 fi
 
-LOCATION="/Applications/"
-
 # Get the absolute path of the directory containing this script
 dir=$(unset CDPATH && cd "$(dirname "$0")" && echo $PWD)
 BUILD=$(date +%Y%m%d%H%M)
@@ -19,6 +17,15 @@ BUILD=$(date +%Y%m%d%H%M)
 /bin/chmod -R o+r "${dir}/payload/"
 /bin/chmod +x "${dir}/scripts/"
 
+#turns preinstall and postinstall files into executables if they exist
+if [ -e "${dir}/scripts/preinstall" ];then
+    chmod a+x "${dir}/scripts/preinstall"
+fi
+
+if [ -e "${dir}/scripts/postinstall" ];then
+    chmod a+x "${dir}/scripts/postinstall"
+fi
+
 #clear the .files  from the folders
 /usr/bin/find "${dir}" -name .DS_Store -delete
 /usr/bin/find "${dir}/payload" -name .DS_Store -delete
@@ -26,24 +33,20 @@ BUILD=$(date +%Y%m%d%H%M)
 /usr/bin/find "${dir}/scripts/" -name .DS_Store -delete
 /usr/bin/find "${dir}/scripts/" -name .keep -delete
 
-if [ ! -s ./payload/*.app ];
-then
-    echo "Copy the .app file into the payload folder before run the script"
-    exit
-fi
-    
-if [ ${BUNDLEISRELOCATABLE} == 0 ];then
-    
-    # Build package that force the .app file to be instaled in the /Applications folder
-    pkgbuild --analyze --root "${dir}/payload/" "${dir}/Info.plist"
-    plutil -replace BundleIsRelocatable -bool NO "${dir}/Info.plist"
-    pkgbuild --identifier "${BUNDLEID}" --component-plist "${dir}/Info.plist" --root "${dir}/payload/"  --install-location "${LOCATION}" --scripts "${dir}/scripts" --version "${VERSION}" "${dir}/${BUNDLEID}-${BUILD}.pkg"
-    rm "${dir}/Info.plist"
-        
-        
+if [ ! -s ./payload/*.app ];then
+    # Build package that contains only scripts
+    pkgbuild --identifier "${BUNDLEID}" --nopayload --scripts "${dir}/scripts/" --version "${VERSION}" "${dir}/${BUNDLEID}-${BUILD}.pkg"
 else
-    
-    # Build package that replace the .app if the machine have one with the seme bundleid alreadyand if not force the .app file to be instaled in the /Applications folder
-    pkgbuild --identifier "${BUNDLEID}" --root "${dir}/payload/"  --install-location "${LOCATION}" --scripts "${dir}/scripts" --version "${VERSION}" "${dir}/${BUNDLEID}-${BUILD}.pkg"
-        
+    LOCATION="/Applications/"
+
+    if [ ${BUNDLEISRELOCATABLE} == 0 ];then
+        # Build package that force the .app file to be instaled in the /Applications folder
+        pkgbuild --analyze --root "${dir}/payload/" "${dir}/Info.plist"
+        plutil -replace BundleIsRelocatable -bool NO "${dir}/Info.plist"
+        pkgbuild --identifier "${BUNDLEID}" --component-plist "${dir}/Info.plist" --root "${dir}/payload/"  --install-location "${LOCATION}" --scripts "${dir}/scripts/" --version "${VERSION}" "${dir}/${BUNDLEID}-${BUILD}.pkg"
+        rm "${dir}/Info.plist"
+    else
+        # Build package that replace the .app if the machine have one with the seme bundleid alreadyand if not force the .app file to be instaled in the /Applications folder
+        pkgbuild --identifier "${BUNDLEID}" --root "${dir}/payload/"  --install-location "${LOCATION}" --scripts "${dir}/scripts/" --version "${VERSION}" "${dir}/${BUNDLEID}-${BUILD}.pkg"    
+    fi
 fi
