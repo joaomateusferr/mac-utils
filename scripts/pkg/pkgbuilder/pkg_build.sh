79d6c537fafd54e22428e3ac72dd1183a08d3e1f
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-08 16:10:34 -0300

    add developer notes

--- scripts/pkg/pkgbuilder/pkg_build.sh
+++ scripts/pkg/pkgbuilder/pkg_build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+
+#Developer notes
+#In the case of a package containing .app files, usually the LOCATION parameter that should be used is /Applications/
+
+#In case the BUNDLE_IS_RELOCATABLE parameter is 0 if the .app already exists on the mac, the LOCATION parameter will be ignored and the .app will be replaced at the location it is on the mac
+#Otherwise, the .app will be placed at the location specified in the LOCATION parameter independent where this .app is located on the mac
+
+#In case you want to sign a package the Developer ID Installer certificate has to be installed on the mac that is running this script for it to work properly
+#To find out how to have your Developer ID Installer certificate and learn more about the Mac Gatekeeper check the link https://developer.apple.com/developer-id/
+#The DEVELOPER_ID_INSTALLER parameter must contain only the id following the pattern *******
+
 BUNDLEID=$1
 VERSION=$2
-BUNDLE_IS_RELOCATABLE=$3
-LOCATION=$4 #/Applications/
-DEVELOPER_ID_INSTALLER="" #*******
+LOCATION=$3
+BUNDLE_IS_RELOCATABLE=$4
+DEVELOPER_ID_INSTALLER=$5
 
 if [ -z $BUNDLEID ] || [ -z $VERSION ];then
-    echo "Please, use the scripts arguments pattern:"
+    echo "Please, use the use at least this scripts arguments:"
     echo "BUNDLEID (com.app...) VERSION (pkg version)"
 else
     # Get the absolute path of the directory containing this script
@@ -47,12 +58,16 @@ else
         if [ -z $LOCATION ] ;then
 
             echo "If there is a payload there must be an installation location"
+            echo "Please, use the use at least this scripts arguments:"
+            echo "BUNDLEID (com.app...) VERSION (pkg version) LOCATION (/Applications/ for example)"
             exit
         fi
 
         if [ -z $BUNDLE_IS_RELOCATABLE ] ;then
 
-            echo "If there is a payload there must be an BUNDLE_IS_RELOCATABLE (1 or 0)"
+            echo "If there is a payload the parameter BUNDLE_IS_RELOCATABLE has to be defined"
+            echo "Please, use the use at least this scripts arguments:"
+            echo "BUNDLEID (com.app...) VERSION (pkg version) LOCATION (/Applications/ for example) BUNDLE_IS_RELOCATABLE (1 or 0)"
             exit
         fi
 
@@ -90,6 +105,9 @@ else
     fi
 
 fi
+
+echo $COMMAND
+exit
 
 eval $COMMAND
 
