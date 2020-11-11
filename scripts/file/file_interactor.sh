a7847fd0b158a3ae4efb3625722e99f8aa3e2be2
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-11 20:52:41 -0300

    update

--- scripts/file/file_interactor.sh
+++ scripts/file/file_interactor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DIR='/tmp' #path to the directory containing the files
+DIR=$1 #path to the directory containing the files
 
 #If nullglob is set, Bash allows filename patterns which match no files to expand to a null string, rather than themselves.
 #to understand this -> https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 
-shopt -s nullglob
+shopt -s nullglob #I still haven't found a way to disable the shopt -s nullglob after using it
 
-for FILE in $DIR/*.{jpg,cr2,conf}; do
+for FILE in $DIR/*.{jpg,png,conf}; do
   #iterate the file here
   echo "$FILE - Iterated"
 done
