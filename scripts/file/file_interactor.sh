42a524d1beb3ba6b679c08a892935d6331bcf8eb
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-11 20:40:27 -0300

    update file interactor

--- scripts/file/file_interactor.sh
+++ scripts/file/file_interactor.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-DIR=$1 #path to the directory containing the files
-
-cd $DIR
+DIR='/tmp' #path to the directory containing the files
 
 #If nullglob is set, Bash allows filename patterns which match no files to expand to a null string, rather than themselves.
 #to understand this -> https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 
-shopt -s nullglob 
+shopt -s nullglob
 
-for FILE in *.{jpg,cr2,conf}; do
+for FILE in $DIR/*.{jpg,cr2,conf}; do
   #iterate the file here
   echo "$FILE - Iterated"
 done
