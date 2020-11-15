ca3026421d07f65fce08fee5847793f39ab9469e
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2020-11-15 16:03:16 -0300

    fix validation

--- scripts/pkg/pkg_get_info_single_app.sh
+++ scripts/pkg/pkg_get_info_single_app.sh
@@ -9,46 +9,53 @@ PATH_TO_PKG='/Users/joaoferreira/Downloads/GoogleChrome.pkg'
 
 FOLDER_TO_EXTRACT_TO='/private/tmp/extracted_pkg'
 
-#if [ -e "$FOLDER_TO_EXTRACT_TO" ];then
-    #rm -rf "$FOLDER_TO_EXTRACT_TO"
-#fi
+if [ -e "$FOLDER_TO_EXTRACT_TO" ];then
+    rm -rf "$FOLDER_TO_EXTRACT_TO"
+fi
 
-#pkgutil --expand-full $PATH_TO_PKG $FOLDER_TO_EXTRACT_TO
+pkgutil --expand-full $PATH_TO_PKG $FOLDER_TO_EXTRACT_TO
 
-cd $FOLDER_TO_EXTRACT_TO
+if [ $? -eq 0 ];then
 
-if [ ! -e "$FOLDER_TO_EXTRACT_TO"/*.pkg ];then #need to fix it
-    echo 'There are no .pkg file on the folder'
-else
+    cd $FOLDER_TO_EXTRACT_TO
 
-    for PKG in $FOLDER_TO_EXTRACT_TO/*.pkg; do
-              
-        PKG_PATH=${PKG// /\\ }
-        eval "cd $PKG_PATH/Payload"
+    PKG_FILE=$(ls | grep '\.pkg$' | head -1)
 
-        for APP in *.app; do  
+    if [ -z "$PKG_FILE" ];then
+        echo 'No .pkg file found in the original pkg'
+    else
+
+        for PKG in $FOLDER_TO_EXTRACT_TO/*.pkg; do
                 
-            APP_PATH=${APP// /\\ }
-            eval "cd $APP_PATH"
-                
-            APP_DIR=$(pwd)
-            APP_DIR_PATH=${APP_DIR// /\\ }
-                
-            APP_NAME=$APP
-            APP_BUNDLE=$(eval "defaults read $APP_DIR_PATH/Contents/Info.plist CFBundleIdentifier")
-            APP_VERSION=$(eval "defaults read $APP_DIR_PATH/Contents/Info.plist CFBundleShortVersionString")
+            PKG_PATH=${PKG// /\\ }
+            eval "cd $PKG_PATH/Payload"
 
-            echo '-----------------------------------------'    
-            echo 'App Info: '
-            echo "Name: $APP_NAME"
-            echo "Bundle: $APP_BUNDLE"
-            echo "Version: $APP_VERSION"
-            echo '-----------------------------------------' 
+            for APP in *.app; do  
+                    
+                APP_PATH=${APP// /\\ }
+                eval "cd $APP_PATH"
+                    
+                APP_DIR=$(pwd)
+                APP_DIR_PATH=${APP_DIR// /\\ }
+                    
+                APP_NAME=$APP
+                APP_BUNDLE=$(eval "defaults read $APP_DIR_PATH/Contents/Info.plist CFBundleIdentifier")
+                APP_VERSION=$(eval "defaults read $APP_DIR_PATH/Contents/Info.plist CFBundleShortVersionString")
+
+                echo '-----------------------------------------'    
+                echo 'App Info: '
+                echo "Name: $APP_NAME"
+                echo "Bundle: $APP_BUNDLE"
+                echo "Version: $APP_VERSION"
+                echo '-----------------------------------------' 
+                cd ..
+            done
             cd ..
         done
         cd ..
-    done
-    cd ..
-    cd ..
-    #rm -rf "$FOLDER_TO_EXTRACT_TO"
+        cd ..
+        rm -rf "$FOLDER_TO_EXTRACT_TO"
+    fi
+else
+    echo 'Error, PKG ...'
 fi
