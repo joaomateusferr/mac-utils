01fa0bcacf73bf81a84ab4d350c5da4f450a65f2
joaomateusferr <47743004+joaomateusferr@users.noreply.github.com>
2021-01-02 23:03:35 -0300

    add change_google_chrome_IncognitoModeAvailability

---
+++ scripts/change_google_chrome_IncognitoModeAvailability.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Developer notes
+#For this script to work completely the device needs to be restarted
+
+#IncognitoModeAvailability values
+#0 -> disable
+#1 -> enable
+#2 -> enforce
+
+INCOGNITOMODEAVAILABILITY=$1
+USER=$2
+
+if [ -z "$USER" ];then
+    USER=$(ls -l /dev/console | awk {'print $3'})
+else
+    id $USER > /dev/null 2>&1
+
+    if [ $? -ne 0 ] ; then
+        echo 'Desired user not found ... exiting'
+        exit
+    fi
+fi
+
+if [ $USER == 'root' ]; then
+    echo 'Someone needs to be logged in the device for this script to work. Try again later.'
+else
+    su $USER -c "defaults -currentHost write com.google.chrome IncognitoModeAvailability -integer $INCOGNITOMODEAVAILABILITY"
+
+    if [ $? -eq 0 ]; then
+        echo 'Google Chrome IncognitoModeAvailability changed, please restart the device for the settings to be applied.'
+    else 
+       echo 'Error while trying to disable screensaver.'    
+    fi
+fi
